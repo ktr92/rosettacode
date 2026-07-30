7b073deff00c14ef12033952f58bea3b424a0fcd
ktr92 <kudashevtim@gmail.com>
2026-07-30 23:33:25 +0300

    [FIX] Two Sum

--- leetcode/twoSum.ts
+++ leetcode/twoSum.ts
@@ -1,24 +1,29 @@
-function twoSum(nums: number[], target: number): number[] {
-  if (nums.length < 2 || nums.length > Math.pow(10, 4))
-    throw new Error("2 <= nums.length <= 10^4");
-  if (target < Math.pow(-10, 9) || target > Math.pow(10, 9))
-    throw new Error("-10^9 <= target <= 10^9");
+function twoSum(nums: number[], target: number): number[] { 
 
   let result: number[] = [];
-  if (nums.length > 2 && nums.length % 2 !== 0) nums.push(0);
-
-  const LEN = nums.length > 2 ? nums.length / 2 : nums.length;
-  for (let i = 0; i < LEN; i++) {
-     if (nums[i] && nums[i + LEN - 1]) {
-      if (nums[i] + nums[i + LEN - 1] === target) {
-        result = [i, i + LEN - 1]
+  const resultMap = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    if (typeof nums[i] !== "undefined") {
+      const sought = resultMap.get(nums[i]);
+      if (sought && sought.value === nums[i]) {
+        result = [Math.min(i, sought.index), Math.max(i, sought.index)];
+        break;
       }
+
+      const value = target - nums[i];
+      resultMap.set(value, {
+       value,
+       index: i,
+       num: nums[i]
+      });
+
     }
-  } 
-  return result
+  }
+  return result;
 }
 
-const nums = [3,2,3];
-const target = 6;
+const nums = [-1,-2,-3,-4,-5]
 
-console.log(twoSum(nums, target))
+const target = -8;
+
+console.log(twoSum(nums, target));
