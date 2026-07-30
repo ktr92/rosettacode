2f7581b8d0a7ef05a070f3d0ff5642b23f0dbfdd
unknown <kudashevtim@gmail.com>
2026-07-30 15:25:07 +0300

    LRUcache

--- leetcode/LRUcache.ts
+++ leetcode/LRUcache.ts
@@ -86,8 +86,8 @@ class LRUCache {
 
     if (newHead) {
       if (this.tail.key === newHead.key && newHead.prev) {
-        this.tail = newHead.prev
-        this.tail.next = null
+        this.tail = newHead.prev;
+        this.tail.next = null;
       }
       this.head.prev = newHead;
       newHead.next = this.head;
@@ -99,15 +99,18 @@ class LRUCache {
   private deleteItem(key: number) {
     const item = this.cache.get(key);
     if (item) {
+      // change next item's prev option
       if (item.next) {
         item.next.prev = item.prev;
-      } else {
-        if (item.prev) {
-          this.tail =  item.prev;
-        } 
       }
+      // change prev item's next option
       if (item.prev) {
         item.prev.next = item.next;
+      }
+      // change tail
+      if (this.tail.key === key && item.prev) {
+        this.tail = item.prev;
+        this.tail.next = null;
       } 
       this.cache.delete(key);
     }
@@ -128,16 +131,14 @@ class LRUCache {
     if (cacheSize > 0) {
       const exist = this.cache.get(key);
       if (typeof exist !== "undefined") {
-        this.deleteItem(key)
-        this.addItem(key, value);
-        this.changeHead(key);
+        this.deleteItem(key);
       } else {
         if (cacheSize >= this.capacity) {
-          this.deleteItem(this.tail.key)
+          this.deleteItem(this.tail.key);
         }
-        this.addItem(key, value);
-        this.changeHead(key);
       }
+      this.addItem(key, value);
+      this.changeHead(key);
     } else {
       this.addItem(key, value);
       const newel = this.cache.get(key);
@@ -150,10 +151,231 @@ class LRUCache {
   }
 }
 
-const command = ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+const command = [
+  "LRUCache",
+  "put",
+  "put",
+  "put",
+  "put",
+  "put",
+  "get",
+  "put",
+  "get",
+  "get",
+  "put",
+  "get",
+  "put",
+  "put",
+  "put",
+  "get",
+  "put",
+  "get",
+  "get",
+  "get",
+  "get",
+  "put",
+  "put",
+  "get",
+  "get",
+  "get",
+  "put",
+  "put",
+  "get",
+  "put",
+  "get",
+  "put",
+  "get",
+  "get",
+  "get",
+  "put",
+  "put",
+  "put",
+  "get",
+  "put",
+  "get",
+  "get",
+  "put",
+  "put",
+  "get",
+  "put",
+  "put",
+  "put",
+  "put",
+  "get",
+  "put",
+  "put",
+  "get",
+  "put",
+  "put",
+  "get",
+  "put",
+  "put",
+  "put",
+  "put",
+  "put",
+  "get",
+  "put",
+  "put",
+  "get",
+  "put",
+  "get",
+  "get",
+  "get",
+  "put",
+  "get",
+  "get",
+  "put",
+  "put",
+  "put",
+  "put",
+  "get",
+  "put",
+  "put",
+  "put",
+  "put",
+  "get",
+  "get",
+  "get",
+  "put",
+  "put",
+  "put",
+  "get",
+  "put",
+  "put",
+  "put",
+  "get",
+  "put",
+  "put",
+  "put",
+  "get",
+  "get",
+  "get",
+  "put",
+  "put",
+  "put",
+  "put",
+  "get",
+  "put",
+  "put",
+  "put",
+  "put",
+  "put",
+  "put",
+  "put",
+];
 
-
-const params = [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+const params = [
+  [10],
+  [10, 13],
+  [3, 17],
+  [6, 11],
+  [10, 5],
+  [9, 10],
+  [13],
+  [2, 19],
+  [2],
+  [3],
+  [5, 25],
+  [8],
+  [9, 22],
+  [5, 5],
+  [1, 30],
+  [11],
+  [9, 12],
+  [7],
+  [5],
+  [8],
+  [9],
+  [4, 30],
+  [9, 3],
+  [9],
+  [10],
+  [10],
+  [6, 14],
+  [3, 1],
+  [3],
+  [10, 11],
+  [8],
+  [2, 14],
+  [1],
+  [5],
+  [4],
+  [11, 4],
+  [12, 24],
+  [5, 18],
+  [13],
+  [7, 23],
+  [8],
+  [12],
+  [3, 27],
+  [2, 12],
+  [5],
+  [2, 9],
+  [13, 4],
+  [8, 18],
+  [1, 7],
+  [6],
+  [9, 29],
+  [8, 21],
+  [5],
+  [6, 30],
+  [1, 12],
+  [10],
+  [4, 15],
+  [7, 22],
+  [11, 26],
+  [8, 17],
+  [9, 29],
+  [5],
+  [3, 4],
+  [11, 30],
+  [12],
+  [4, 29],
+  [3],
+  [9],
+  [6],
+  [3, 4],
+  [1],
+  [10],
+  [3, 29],
+  [10, 28],
+  [1, 20],
+  [11, 13],
+  [3],
+  [3, 12],
+  [3, 8],
+  [10, 9],
+  [3, 26],
+  [8],
+  [7],
+  [5],
+  [13, 17],
+  [2, 27],
+  [11, 15],
+  [12],
+  [9, 19],
+  [2, 15],
+  [3, 16],
+  [1],
+  [12, 17],
+  [9, 1],
+  [6, 19],
+  [4],
+  [5],
+  [5],
+  [8, 1],
+  [11, 7],
+  [5, 2],
+  [9, 28],
+  [1],
+  [2, 2],
+  [7, 4],
+  [4, 22],
+  [7, 24],
+  [9, 26],
+  [13, 28],
+  [11, 26],
+];
 
 if (typeof params[0] !== "undefined" && typeof params[0][0] !== "undefined") {
   const lRUCache = new LRUCache(params[0][0]);
@@ -171,6 +393,7 @@ if (typeof params[0] !== "undefined" && typeof params[0][0] !== "undefined") {
       }
     }
     console.log(lRUCache.getSize());
+
     return val;
   });
 
