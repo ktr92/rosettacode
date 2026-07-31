08b8c4d7e7bc83f8348577154837860fca9e6073
ktr92 <kudashevtim@gmail.com>
2026-07-31 18:22:14 +0300

    [ADD] Merge Intervals

--- leetcode/merge_intervals.ts
+++ leetcode/merge_intervals.ts
@@ -2,37 +2,59 @@ function merge(intervals: number[][]): number[][] {
   const result: number[][] = [];
   const indexMap = new Map();
   for (let i = 0; i < intervals.length; i++) {
+    const first_start = intervals[i][0];
+    const first_end = intervals[i][1];
     for (let j = 0; j < intervals.length; j++) {
+      const second_start = intervals[j][0];
+      const second_end = intervals[j][1];
       if (i !== j) {
-        if (intervals[i][0] <= intervals[j][0]) {
-          if (
-            intervals[i][1] <= intervals[j][1] &&
-            intervals[i][1] >= intervals[j][0]
-          ) {
-            result.push([intervals[i][0], intervals[j][1]]);
-            indexMap.set(i, true);
-            indexMap.set(j, true);
+        if (first_start < second_start) {
+          if (first_end <= second_end && first_end >= second_start) {
+            if (!indexMap.has(j) && !indexMap.has(i)) {
+              result.push([first_start, second_end]);
+              indexMap.set(i, true);
+              indexMap.set(j, true);
+            }
           } else {
             if (!indexMap.has(j) && !indexMap.has(i)) {
-              result.push([intervals[i][0], intervals[i][1]]);
+              result.push([first_start, first_end]);
+               indexMap.set(i, true);
+                indexMap.set(j, true);
             }
           }
         }
-        if (intervals[i][0] > intervals[j][0]) {
-          if (
-            intervals[i][1] >= intervals[j][1] &&
-            intervals[i][1] <= intervals[j][0]
-          ) {
-            if (intervals[i][0] === intervals[j][1]) {
-              result.push([intervals[j][0], intervals[i][1]]);
-              indexMap.set(i, true);
-              indexMap.set(j, true);
-            } else {
-              result.push([intervals[j][0], intervals[i][1]]);
+        if (first_start > second_start) {
+          if (first_end >= second_end && first_end >= second_start) {
+              if (!indexMap.has(j) && !indexMap.has(i)) {
+                result.push([second_start, first_end]);
+                indexMap.set(i, true);
+                indexMap.set(j, true);
+              }
+        
+          } else {
+            if (!indexMap.has(j) && !indexMap.has(i)) {
+              result.push([first_start, first_end]);
+               indexMap.set(i, true);
+                indexMap.set(j, true);
+            }
+          }
+        }
+        if (first_start === second_end) {
+          if (first_end > second_start) {
+            if (!indexMap.has(j) && !indexMap.has(i)) {
+              result.push([second_start, first_end]);
               indexMap.set(i, true);
               indexMap.set(j, true);
             }
-          } 
+          }
+        }
+        if (!indexMap.has(i)) {
+          result.push([first_start, first_end]);
+           indexMap.set(i, true);
+        }
+        if (!indexMap.has(j)) {
+          result.push([second_start, second_end]);
+                indexMap.set(j, true);
         }
       }
     }
@@ -41,8 +63,10 @@ function merge(intervals: number[][]): number[][] {
 }
 
 const intervals = [
-  [4, 7],
-  [1, 4],
+  [1, 3],
+  [2, 6],
+  [8, 10],
+  [15, 18],
 ];
 
 console.log(merge(intervals)); // [[1,7]]
