4063d1eb980cd985c8997b30ae6c4262e8d27236
unknown <kudashevtim@gmail.com>
2026-08-01 10:47:45 +0300

    [UPD] merge intervals

--- leetcode/merge_intervals.ts
+++ leetcode/merge_intervals.ts
@@ -1,23 +1,25 @@
 function merge(intervals: number[][]): number[][] {
-  const fillRange = (start: number, end: number) =>
-    Array.from({ length: end - start + 1 }, (_, index) => start + index);
+  const fillRange = (start: number, end: number) => {
+    if (start === end) {
+      return Array.from({ length: 2 }, (_, index) => start);
+    }
+    return Array.from({ length: end - start + 1 }, (_, index) => start + index);
+  }
+    
 
   const flat: number[] = [];
 
   const edgesMap = new Map();
 
   intervals.forEach((item: number[], index: number) => {
-    edgesMap.set(item[0], {
-      max: false,
-      min: true,
-    });
-    edgesMap.set(item[1], {
-      max: true,
-      min: false,
+    edgesMap.set(`${item[0]}${item[1]}`, {
+      max: item[1],
+      min: item[0],
+      key: index
     });
     flat.push(...fillRange(item[0] as number, item[1] as number));
   });
-  const sorted: number[] = [...new Set(flat)].sort((a: number, b: number) =>
+  const sorted: number[] = flat.sort((a: number, b: number) =>
     a > b ? 1 : -1,
   );
 
@@ -27,28 +29,35 @@ function merge(intervals: number[][]): number[][] {
   console.log(edgesMap);
 
   for (let i = 0; i < sorted.length; i++) {
-    if (sorted[i] && sorted[i + 1]) {
+
+    if (sorted[i] === sorted[i + 1] && edgesMap.has(`${sorted[i]}${sorted[i+1]}`)) {
+      const min = edgesMap.get(`${sorted[i]}${sorted[i+1]}`).min
+      const max = edgesMap.get(`${sorted[i]}${sorted[i+1]}`).max
+      if (min === max) {
+         merged.push([sorted[i] as number, sorted[i+1] as number]);
+          start = i + 2
+      }
+    } else if (sorted[i] && sorted[i + 1]) {
       const leftEdge = edgesMap.has(sorted[i]) ? edgesMap.get(sorted[i]).max : false;
       const rightEdge = edgesMap.has(sorted[i + 1]) ? edgesMap.get(sorted[i + 1]).min : false;
       const isEdge = leftEdge && rightEdge;
       const isSiblings = (sorted[i + 1] as number) - (sorted[i] as number) > 1;
 
-      console.log(i, sorted[i], sorted[i + 1], isEdge, isSiblings);
-
+/*       console.log(i, sorted[i], sorted[i + 1], isEdge, isSiblings);
+ */
       if (i < sorted.length - 1 && (isSiblings || isEdge)) {
         merged.push([sorted[start] as number, sorted[i] as number]);
         start = i + 1;
       }
     } else {
       if (sorted[i] !== sorted[start]) {
-      merged.push([sorted[start] as number, sorted[i] as number]);
-
+       merged.push([sorted[start] as number, sorted[i] as number]);
       }
     }
   }
   return merged;
 }
 
-const intervals = [[1,4],[0,4]]
+const intervals = [[1,4],[0,0]]
 
-console.log(merge(intervals)); // [[0,4]]
+console.log(merge(intervals)); // [[0,0],[1,4]]
