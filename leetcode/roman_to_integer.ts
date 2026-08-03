8ad6b82737db85dd905284e3780db3d4fa869574
unknown <kudashevtim@gmail.com>
2026-08-03 17:13:02 +0300

    jest

--- leetcode/roman_to_integer.ts
+++ leetcode/roman_to_integer.ts
@@ -1,5 +1,16 @@
 function romanToInt(s: string): number {
-    return 3
+ const symbolsMap = new Map([
+  ["I", 1],
+  ["V", 5],
+  ["X", 10],
+  ["L", 50],
+  ["C", 100],
+  ["D", 500],
+  ["M", 1000],
+]);
+
+ 
+ return 3
 };
 
 module.exports = romanToInt;
