ed8b8b6c13068583befa620320304116b163bf83
ktr92 <kudashevtim@gmail.com>
2026-08-11 18:36:09 +0300

    [UPD] lengthOfLongestSubstring hashmap

--- leetcode/lengthOfLongestSubstring.ts
+++ leetcode/lengthOfLongestSubstring.ts
@@ -7,17 +7,14 @@ function lengthOfLongestSubstring(s: string): number {
   for (let j = 0; j < arr.length; j++) {
     if (!hashMap.has(arr[j])) {
       tmp.push(arr[j] as string);
-      hashMap.set(arr[j], j)
+      hashMap.set(arr[j], j);
     } else {
       if (tmp.length > unique.length) {
         unique = [...tmp];
       }
-      const start = hashMap.get(arr[j])
-      hashMap.clear()
-        tmp.length = 0 
-        j = start 
-    
-      
+      j = hashMap.get(arr[j]);
+      hashMap.clear();
+      tmp.length = 0;
     }
   }
 
