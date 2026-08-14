da0c543b605960df3b8847e5ff7c22444cb6f999
unknown <kudashevtim@gmail.com>
2026-08-14 18:24:14 +0300

    [ADD] robArray

--- leetcode/robArray.ts
+++ leetcode/robArray.ts
@@ -1,52 +1,36 @@
 function rob(nums: number[]): number {
-  function indexOfMax(arr) {
-    if (arr.length === 0) {
-      return -1;
-    }
+  
 
-    var max = arr[0];
-    var maxIndex = 0;
+  let skip = null;
+  let last = 0;
+  let summ = nums[0];
 
-    for (var i = 1; i < arr.length; i++) {
-      if (arr[i] > max) {
-        maxIndex = i;
-        max = arr[i];
-      }
-    }
-
-    return maxIndex;
-  }
-
-  const hashMap = new Map();
-  let summ = 0;
-
-  function addIndex(i: number) {
-    hashMap.set(i, i);
-    nums.splice(i, 1, -Infinity);
-  }
-
-  for (let i = 0; i < nums.length; i++) {
-    let maxIndex = indexOfMax(nums);
-    if (!hashMap.has(maxIndex)) {
-     if (nums[maxIndex + 1] && nums[maxIndex - 1]) {
-      if (nums[maxIndex + 1] + nums[maxIndex - 1] > nums[maxIndex]) {
-       addIndex(maxIndex);
-       continue
-      }
-     }
-      summ += nums[maxIndex];
-
-      addIndex(maxIndex);
-
-      if (nums[maxIndex + 1]) {
-        addIndex(maxIndex + 1);
-      }
-      if (nums[maxIndex - 1]) {
-        addIndex(maxIndex - 1);
-      }
+  const step = (a, b, c) => {
+    if (b > a + c) {
+      return b;
     } else {
-      nums.splice(maxIndex, 1, -Infinity);
+      return c;
     }
+  };
+
+  let skipValue = 0;
+
+  let prev = 0
+
+  for (let i = 1; i < nums.length; i++) {
+    skipValue = typeof skip === 'number' ? nums[skip] : 0;
+
+    if (nums[i] + skipValue > nums[last] + prev) {
+      prev = skipValue
+      summ += nums[i] + skipValue - nums[last];
+      skip = last;
+      last = i;
+    } else {
+      skip = i;
+      skipValue = nums[i]
+    }
+
+    console.log('skip: ', skip, ' last: ', last, ' summ: ', summ)
   }
 
   return summ;
