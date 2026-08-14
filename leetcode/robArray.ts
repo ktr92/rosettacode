3be4151499260406a20d4ac6fef9696f18263977
unknown <kudashevtim@gmail.com>
2026-08-14 19:03:51 +0300

    [ADD] robArray

--- leetcode/robArray.ts
+++ leetcode/robArray.ts
@@ -1,36 +1,28 @@
 function rob(nums: number[]): number {
   
 
-  let skip = null;
-  let last = 0;
-  let summ = nums[0];
+  let skip = nums[0] > nums[1] ? 1 : 0;
+  let last = nums[0] > nums[1] ? 0 : 1;
 
-  const step = (a, b, c) => {
-    if (b > a + c) {
-      return b;
-    } else {
-      return c;
-    }
-  };
-
-  let skipValue = 0;
-
+  let skipValue = nums[skip];
   let prev = 0
+  let summ = nums[last];
 
-  for (let i = 1; i < nums.length; i++) {
+  for (let i = 2; i < nums.length; i++) {
+    console.log('skip: ', skip, ' last: ', last, ' summ: ', summ)
+
     skipValue = typeof skip === 'number' ? nums[skip] : 0;
-
+   
     if (nums[i] + skipValue > nums[last] + prev) {
-      prev = skipValue
-      summ += nums[i] + skipValue - nums[last];
+      summ += nums[i] - nums[last] + skipValue - prev;
       skip = last;
       last = i;
     } else {
       skip = i;
       skipValue = nums[i]
     }
+    prev = nums[last - 2]
 
-    console.log('skip: ', skip, ' last: ', last, ' summ: ', summ)
   }
 
   return summ;
