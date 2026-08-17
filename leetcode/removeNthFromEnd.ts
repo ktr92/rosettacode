767733b50c57aa597655fd1a351603332056a306
ktr92 <kudashevtim@gmail.com>
2026-08-17 19:01:28 +0300

    [UPD] lengthOfLongestSubstring hashmap

--- leetcode/removeNthFromEnd.ts
+++ leetcode/removeNthFromEnd.ts
@@ -17,28 +17,22 @@ function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
 
   // вышли за пределы, двигаться некуда, удаляем вершину
   if (!right) {
-   return left.next
+    return left.next;
   }
 
-  let prev = null
-
   while (right.next) {
-    prev = left
     left = left.next;
     right = right.next;
-    console.log(prev, left, right)
   }
 
   // дошли до конца и между левым и правым ничего нет - удаляем хвост
-
   if (left?.next?.next === null) {
-   left.next = null;
-   return head
-  } 
+    left.next = null;
+    return head;
+  }
 
-  prev.next = left?.next
+  left.next = left?.next?.next;
 
-  console.log(JSON.stringify(head))
   return head;
 }
 
