a16cadfa04596f6de02518fed72928cdcd94193f
unknown <kudashevtim@gmail.com>
2026-08-17 17:26:57 +0300

    [ADD] removeNthFromEnd

--- leetcode/removeNthFromEnd.ts
+++ leetcode/removeNthFromEnd.ts
@@ -20,12 +20,13 @@ function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
    return left.next
   }
 
+  let prev = null
 
-
-  while (right) {
-    left.next.prev = left
-    left = left?.next;
+  while (right.next) {
+    prev = left
+    left = left.next;
     right = right.next;
+    console.log(prev, left, right)
   }
 
   // дошли до конца и между левым и правым ничего нет - удаляем хвост
@@ -35,7 +36,9 @@ function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
    return head
   } 
 
-  left.prev.next = right
+  prev.next = left?.next
+
+  console.log(JSON.stringify(head))
   return head;
 }
 
