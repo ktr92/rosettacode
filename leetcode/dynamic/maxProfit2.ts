93fa4e10e221674afe5aedcb347c985d1fd70524
ktr92 <kudashevtim@gmail.com>
2026-08-20 18:49:17 +0300

    [ADD] maxProfit2

--- leetcode/dynamic/maxProfit2.ts
+++ leetcode/dynamic/maxProfit2.ts
@@ -1,17 +1,19 @@
 function maxProfit2(prices: number[]): number {
-  // dp[i][j] - текущий профит если покупать в дату i
+  // dp[i][j] - текущий профит если покупать в дату i и продать в дату j
   const dp = [];
-  let buy = 0;
   for (let i = 0; i < prices.length; i++) {
-    dp[i] = [];
-    if (j <= i) {
-      dp[i][j] = 0;
-    } else {
-      for (let j = prices.length - 1; j >= 0; j--) {
-      dp[i][j] = prices[j] - prices[i] >= 0 ? prices[j] - prices[i] : 0;
+    for (let j = prices.length - 1; j >= 0; j--) {
+      dp[i] = [];
+        for (let j = prices.length - 1; j >= 0; j--) {
+          if (j < i) {
+            dp[i][j] = 0;
+          } else {
+          dp[i][j] = prices[j] - prices[i] >= 0 ? prices[j] - prices[i] : 0;
+
+          }
+        }
+     
     }
-    }
-   
   }
   console.log(dp);
   return 1;
