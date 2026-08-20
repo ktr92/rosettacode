60997fe49c51c50aa818d33dece1ed1d7e7f7017
unknown <kudashevtim@gmail.com>
2026-08-20 13:59:45 +0300

    mergeAlternately

--- leetcode/dynamic/maxProfit2.ts
+++ leetcode/dynamic/maxProfit2.ts
@@ -4,9 +4,14 @@ function maxProfit2(prices: number[]): number {
   let buy = 0;
   for (let i = 0; i < prices.length; i++) {
     dp[i] = [];
-    for (let j = prices.length - 1; j > i; j--) {
-      dp[i][j] = prices[j] - prices[i];
+    if (j <= i) {
+      dp[i][j] = 0;
+    } else {
+      for (let j = prices.length - 1; j >= 0; j--) {
+      dp[i][j] = prices[j] - prices[i] >= 0 ? prices[j] - prices[i] : 0;
     }
+    }
+   
   }
   console.log(dp);
   return 1;
