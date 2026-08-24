f9b74676ac9aac01a0971b050a217976c6eaa4d6
ktr92 <kudashevtim@gmail.com>
2026-08-24 21:44:30 +0300

    [ADD] structures.1.3

---
+++ structures/queue.js
@@ -0,0 +1,47 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  enqueue(element) {
+    const node = new Node(element);
+    const tail = this.tail;
+    if (tail) {
+     tail.next = node;
+    } else {
+     this.head = node;
+    }
+    this.tail = node;
+  }
+
+  dequeue() {
+    const head = this.head;
+    if (!this.head) {
+     this.tail = null
+     return null
+    }
+    this.head = head.next;
+    return head.value;
+  }
+}
+
+const a = new Queue();
+
+a.enqueue(3);
+a.enqueue(4);
+a.enqueue(5);
+a.enqueue(6);
+a.enqueue(7);
+console.log(a.dequeue());
+
+
+
+console.log(a);
