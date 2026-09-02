0c38ae97826fbd767acbed62e70b6987edecbebc
ktr92 <kudashevtim@gmail.com>
2026-09-02 22:47:17 +0300

    [UPD] longestCommonSubsequence

--- leetcode/dynamic/longestCommonSubsequence.ts
+++ leetcode/dynamic/longestCommonSubsequence.ts
@@ -38,24 +38,19 @@ function longestCommonSubsequence(text1: string, text2: string): number {
   const dp: number[][] = [];
   const len1 = text1.length;
   const len2 = text2.length;
+  dp[0] = [0];
 
-  for (let i = 0; i < len1; i++) {
+  for (let i = 1; i <= len1; i++) {
     dp[i] = [];
-    for (let j = 0; j < len2; j++) {
-      dp[i - 1] = i > 0 ? dp[i - 1] : [0];
-
-      if (text1[i] === text2[j]) {
+    for (let j = 1; j <= len2; j++) {
+      if (text1[i - 1] === text2[j - 1]) {
         dp[i][j] = (dp[i - 1][j - 1] || 0) + 1;
       } else {
-        let prevTopLeft = dp[i - 1][j - 1] || 0;
-        let prevTop = dp[i - 1][j] || 0;
-        let prevLeft = dp[i][j - 1] || 0;
-
-        dp[i][j] = Math.max(prevLeft, prevTop, prevTopLeft);
+        dp[i][j] = Math.max(dp[i - 1][j] || 0, dp[i][j - 1] || 0);
       }
     }
   }
-  return dp[len1 - 1][len2 - 1] || 0;
+  return dp[len1][len2] || 0;
 }
 
 export default longestCommonSubsequence;
