18aef3f4c3d5839db56bfde90adc749e6d243c47
unknown <kudashevtim@gmail.com>
2026-09-07 18:46:07 +0300

    [ADD] 1_8_setZeroes

--- algorithms/chapter1/1_9_isCyclicShift.ts
+++ algorithms/chapter1/1_9_isCyclicShift.ts
@@ -8,8 +8,24 @@ function isCyclicShift(str1: string, str2: string): boolean {
  
  if (str1.length !== str2.length) return false;
 
+ const arr1 = str1.split('')
+ const arr2 = str2.split('')
 
- if (!str1.includes(str2)) return false;
+ let startIndex = 0;
+ while (arr1[startIndex] !== arr2[0] && arr2[startIndex] !== arr1[0] && startIndex < arr1.length && startIndex < arr2.length) {
+  startIndex++
+ }
+ let first = arr1[startIndex] === arr2[0] ? arr1 : arr2;
+ let second = arr1[startIndex] === arr2[0] ? arr2 : arr1;
+
+ let left = 0;
+ let right = startIndex;
+ while (first[right] === second[left] && right < first.length - 1) {
+  right++
+  left++
+ }
+
+ console.log(first[right])
 
  return true;
 } 
