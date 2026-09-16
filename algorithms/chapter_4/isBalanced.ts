07103a63116be04f89e5b03843b78b0082c77395
unknown <kudashevtim@gmail.com>
2026-09-16 12:26:20 +0300

    [ADD] isbalanced

--- algorithms/chapter_4/isBalanced.ts
+++ algorithms/chapter_4/isBalanced.ts
@@ -10,7 +10,21 @@ class TreeNode {
 }
 
 function isBalanced(root: TreeNode | null): boolean {
-  return true;
+  function postOrderTraversal (node: TreeNode | null) {
+    if (node === null) return 0;
+
+    const leftHeight: number = postOrderTraversal(node.left);
+    if (leftHeight === -1) return -1;
+
+    const rightHeight: number = postOrderTraversal(node.right);
+    if (rightHeight === -1) return -1;
+
+    if (Math.abs(leftHeight - rightHeight) > 1) return -1;
+
+    return Math.max(leftHeight, rightHeight) + 1;
+  };
+ 
+  return postOrderTraversal(root) !== -1;
 }
 
 const root = new TreeNode(
@@ -18,5 +32,19 @@ const root = new TreeNode(
   new TreeNode(9, null, null),
   new TreeNode(20, new TreeNode(15), new TreeNode(7)),
 );
+const root2 = new TreeNode(
+  1,
+  new TreeNode(
+    2,
+    new TreeNode(3, 
+    new TreeNode(3, 
+      new TreeNode(4, null, null), 
+      new TreeNode(4, null, null),
+    ), null),
+    null,
+  ),
+  new TreeNode(2, null, null),
+);
 
 console.log(isBalanced(root));
+console.log(isBalanced(root2));
